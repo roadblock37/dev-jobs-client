4f4f0aaf5ee1fe9d7e193d403fccfb5da854c553
Paul Biber <roadblock37@gmail.com>
2023-02-13 13:44:32 -0500

    finished all icon components

--- src/Components/Icons/index.js
+++ src/Components/Icons/index.js
@@ -2,22 +2,27 @@ import React from "react";
 import CheckIcon from "./check";
 import Filter from "./filter";
 import LocationIcon from "./location";
+import MoonIcon from "./moon";
+import SearchIcon from "./search";
 import Sun from "./sun";
 
-
 const Icon = (props) => {
-    switch (props.name) {
-        case "sun":
-            return <Sun {...props}/>;
-        case "filter":
-            return <Filter {...props}/>;
-        case "check":
-            return <CheckIcon {...props}/>;
-        case "location":
-            return <LocationIcon {...props}/>;
-        default:
-            return;
-    }
-}
+  switch (props.name) {
+    case "sun":
+      return <Sun {...props} />;
+    case "filter":
+      return <Filter {...props} />;
+    case "check":
+      return <CheckIcon {...props} />;
+    case "location":
+      return <LocationIcon {...props} />;
+    case "moon":
+      return <MoonIcon {...props} />;
+    case "search":
+      return <SearchIcon {...props} />;
+    default:
+      return;
+  }
+};
 
 export default Icon;
--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Icon from "./Icons";
 
 const SearchBar = () => {
+  // TODO 
+  // add other input fields for search functionality
   return (
     <div className="search-container">
       <input type="text" placeholder="Filter by title..."></input>
