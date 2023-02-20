e1e79b10c600f592297060e0cf47d19e7307f85a
roadblock37 <Roadblock37@gmail.com>
2023-02-20 15:41:24 -0500

    searchbar styled

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Icon from "./Icons";
+import { ReactComponent as Search } from "../Images/desktop/icon-search.svg";
 
 const SearchBar = () => {
-  // TODO 
+  const [text, setText] = useState();
+  // TODO
   // add other input fields for search functionality
+  // setup input fields to send typed text as query
   return (
     <div className="search-container">
-      <input type="text" placeholder="Filter by title..."></input>
-      <Icon name="filter" className="filter"/>
+      <input type="text" placeholder="Filter by title..." onChange={setText}>
+        {text}
+      </input>
+      <Icon name="filter" className="filter" />
+      <div className="search-icon">
+        <Search />
+      </div>
     </div>
   );
 };
